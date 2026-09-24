2d56afd52c49ee0c01463d12fd4be46240ddc63e
jose <josemariagonzalezpavon25@gmail.com>
2026-09-24 11:57:29 -0300

    conexion con la base de datos

--- config/conexion.php
+++ config/conexion.php
@@ -1,27 +1,41 @@
 <?php
-class conectar {
-    protected  $dbh;
-    public function conexion(){
-            try{
-                 $this->dbh = new PDO(
-                    "mysql:host=localhost;dbname;charset=utf8",
-                    "root",
-                    "",
-                    [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                    ]
-                );
-                echo "conexion exitosa a la base de datos";
-                return $this->dbh;
-            }catch(Exeption $e){
-                echo "error DB:".$e->getMessage();
-                die();
-            }
+
+// Clase encargada de realizar la conexión con la base de datos
+class Conectar
+{
+    // Variable que almacenará la conexión PDO
+    protected $dbh;
+
+    // Método para establecer la conexión
+    protected function Conexion()
+    {
+        try {
+            // Crea una nueva conexión con MySQL mediante PDO
+            $this->dbh = new PDO(
+                "mysql:host=localhost;dbname=ventas",
+                "root",
+                ""
+            );
+
+            // Retorna la conexión establecida
+            return $this->dbh;
+
+        } catch (Exception $e) {
+
+            // Muestra el mensaje si ocurre un error de conexión
+            print "Error BD: " . $e->getMessage() . "<br/>";
+
+            // Detiene la ejecución del programa
+            die();
+        }
     }
-    public function set_names(){
-        return $this->dbh->query("SET NAME'utf8'");
+
+    // Método para configurar la codificación de caracteres
+    public function set_names()
+    {
+        // Establece la codificación UTF-8 para la conexión
+        return $this->dbh->query("SET NAMES 'utf8'");
     }
 }
-$conexion = new conectar;
-$conexion->Conexion();
+
 ?>
--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -35,6 +35,7 @@
     <link href="../../public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
     <link href="../../public/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
     <link href="../../public/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
+    <link href="../../public/lib/datatables/jquery.dataTables.css" rel="stylesheet">
 
     <!-- Bracket CSS -->
     <link rel="stylesheet" href="../../public/css/bracket.css">
@@ -689,10 +690,10 @@
         <!-- start you own content here -->
           <div class="br-section-wrapper">
           <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Mantenimiento de productos</h6>
-          <!-- <p class="mg-b-25 mg-lg-b-50">Searching,.</p> -->
+           <p class="mg-b-25 mg-lg-b-50">Searching,.</p>
 
           <div class="table-wrapper">
-            <table id="datatable1" class="table display responsive nowrap">
+            <table id="producto_data" class="table display responsive nowrap">
               <thead>
                 <tr>
                   <th class="wd-15p">Nombre Producto</th>
@@ -701,7 +702,7 @@
                 </tr>
               </thead>
               <tbody>
-                <!-- <tr>
+                 <tr>
                   <td>Gaseosa CocaCola 2L</td>
                   <td><button class=" btn btn-primary">editar</button></td>
                   <td><button class=" btn btn-danger">eliminar</button></td>
@@ -712,7 +713,7 @@
                   <td><button class=" btn btn-outline-primary btn-icon"><div class="fa fa-edit"></div></button></td>
                   <td><button class=" btn btn-outline-danger btn-icon"><div class="fa fa-trash"></div></button></td>
                  
-                </tr> -->
+                </tr> 
               </tbody>
             </table>
           </div><!-- table-wrapper -->
@@ -730,7 +731,34 @@
     <script src="../../public/lib/jquery-ui/jquery-ui.js"></script>
     <script src="../../public/lib/jquery-switchbutton/jquery.switchButton.js"></script>
     <script src="../../public/lib/peity/jquery.peity.js"></script>
-
+    <script src="../../public/lib/datatables/jquery.dataTables.js"></script>
+    <script src="../../public/lib/datatables-responsive/dataTables.responsive.js"></script>
     <script src="../../public/js/bracket.js"></script>
+    <script src="mntproducto.js"></script>
+
+     <!-- <script>
+      $(function(){
+        'use strict';
+
+        $('#producto_data').DataTable({
+          responsive: true,
+          language: {
+            searchPlaceholder: 'Search...',
+            sSearch: '',
+            lengthMenu: '_MENU_ items/page',
+          }
+        });
+
+        $('#producto_data').DataTable({
+          bLengthChange: false,
+          searching: false,
+          responsive: true
+        });
+
+        // Select2
+        $('.dataTables_length select').select2({ minimumResultsForSearch: Infinity });
+
+      });
+    </script> -->
   </body>
 </html>
